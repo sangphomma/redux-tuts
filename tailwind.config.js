24238c0164b2fcea6678683084466498fe0c0189
sangphomma <87381359+sangphomma@users.noreply.github.com>
2023-10-05 02:13:42 +0700

    Besic constructor with tailwind css && redux-toolkit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      Poppins: "Poppins",
+      Sarabun: "Sarabun",
+    },
+    extend: {
+      colors: {
+        Teal: "#2F6C6D",
+        HummingBird: "#d1f1ee",
+        yellow: "#e4d63b",
+        Solitude: "#e9e9ea",
+        gray: "#4B4B4C",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
